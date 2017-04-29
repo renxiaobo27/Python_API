3f8b423ce7b9bb9dd90f0e4aacf590ff97ce1082
renxiaobo27 <renxiaobo27@gmail.com>
2017-04-29 10:25:25 -0700

    Update abstractmedhod.py

--- abstractmedhod.py
+++ abstractmedhod.py
@@ -2,3 +2,25 @@ class Base(object):
     #Abstractmethods
     def parse(self,query):
         raise NotImplementedError
+        
+        
+#class method for facotry pattern
+class Fridge(object):
+    def __init__(self):
+        self.cheese = 'chees'
+        self.veg = "veg"
+
+    def get_cheese(self):
+        return self.cheese
+
+    def get_vegetables(self):
+        return self.veg
+
+
+class Pizza(object):
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    @classmethod
+    def from_fridge(cls, fridge):
+        return cls(fridge.get_cheese() + fridge.get_vegetables())
