4a4165fdaf2efee48a946d2c6e3363a9efd3f6c8
renxiaobo27 <renxiaobo27@gmail.com>
2017-06-15 11:26:53 -0700

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,12 +1,13 @@
 def load_image_from_folder(path):
     full_file_path = []
+    images_names = []
     valid_file_ext = ['.jpg','.png']
     for root,dirs,files in os.walk(path):
         for filename in files:
             ext = os.path.splitext(filename)[1]
-            print (ext)
             if ext.lower() not in valid_file_ext:
                 continue
             filepath =os.path.join(root,filename)
             full_file_path.append(filepath)
-    return full_file_path
+            images_names.append(filename)
+    return full_file_path,images_names
