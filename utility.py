b47409313fd6b673efa00aef91edcb95ea7d79f2
renxiaobo27 <renxiaobo27@gmail.com>
2017-08-25 11:41:39 -0700

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,6 +1,10 @@
+import os
+
+
 def load_image_from_folder(path):
     full_file_path = []
     images_names = []
+    exts = []
     valid_file_ext = ['.jpg','.png']
     for root,dirs,files in os.walk(path):
         for filename in files:
@@ -10,7 +14,36 @@ def load_image_from_folder(path):
             filepath =os.path.join(root,filename)
             full_file_path.append(filepath)
             images_names.append(os.path.splitext(filename)[0])# without ext
-    return full_file_path,images_names
+            exts.append(ext)
+    return full_file_path,images_names,exts
+
+def get_current_subfolers(path):
+    return os.listdir(path)
+
+def get_all_subfolders(path):
+    res=[]
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            res.append(os.path.join(root,dir))
+
+    return res
+#rename images by 05%
+def rename_images_by_Id(folders_path,num_leading_zeros=5,startId=1):
+    image_folder_path = folders_path
+    full_file_path,images_names,exts = load_image_from_folder(image_folder_path)
+    a,b,c = zip(*sorted(zip(full_file_path,images_names,exts)))
+
+    for full_path,image,ext in zip(a,b,c):
+        newId = str(startId).zfill(num_leading_zeros)
+        startId += 1
+        os.rename(full_path,os.path.join(image_folder_path,newId+ext))
+        
+        
+#rename_images_by_Id('/home/tcl-admin/data/videos/training/test2')
+
+#print get_current_subfolers('/home/tcl-admin/data/videos/training')
+#print get_all_subfolders('/home/tcl-admin/data/videos/training')       
+
 
 def make_sure_path_exists(path):
     try:
